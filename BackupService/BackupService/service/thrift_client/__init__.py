cd7bd4a9680812e8f30542fc172b869c3a626457
shi.hao <shi.hao@puscene.com>
2021-06-13 19:22:00 +0800

    xx

--- BackupService/BackupService/service/thrift_client/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding:utf-8 -*-
-# User     shihao
-# Ctime    2021/6/9
--- thrift_server/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\project\python\thrift_server\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
